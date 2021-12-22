e4f96e208c15233305738de59b407b898ee99ba4
remmi755 <90457721+remmi755@users.noreply.github.com>
2021-12-22 17:59:44 +0300

    add changes

--- scripts.js
+++ scripts.js
@@ -1,80 +1,69 @@
-const Watch1 = function () {
-    this.newWatch = document.createElement("div");
-    this.addElement = function () {
+let Watch = function (elem) {
+    this.elem = 'elem'
+    this.newWatch = document.createElement(elem);
+}
+Watch.prototype.addElement = function () {
+    this.newWatch.innerHTML = this.createHhMmSsMsFormat()
+    document.body.prepend(this.newWatch);
+    this.newWatch.className = 'full'
+    this.newWatch.classList.add('styles')
+    this.newWatch.addEventListener('click', this.toggles.bind(this))
+}
+
+Watch.prototype.creatAllFormats = function () {
+    let date = new Date()
+    this.hh = date.getHours();
+    if (this.hh < 10) this.hh = '0' + this.hh;
+    this.mm = date.getMinutes();
+    if (this.mm < 10) this.mm = '0' + this.mm;
+    this.ss = date.getSeconds();
+    if (this.ss < 10) this.ss = '0' + this.ss;
+    this.ms = date.getMilliseconds();
+    if (this.ms < 10) this.ms = '00' + this.ms
+    if (this.ms >= 10 && this.ms < 100) this.ms = '0' + this.ms
+}
+
+Watch.prototype.createHhMmSsMsFormat = function () {
+    this.creatAllFormats()
+    return `${this.hh}:${this.mm}:${this.ss}:${this.ms}`
+}
+
+Watch.prototype.createHhMmSsFormat = function () {
+    this.creatAllFormats()
+    return `${this.hh}:${this.mm}:${this.ss}`
+}
+
+Watch.prototype.creatHhMmFormat = function () {
+    this.creatAllFormats()
+    return `${this.hh}:${this.mm}`
+}
+
+Watch.prototype.render = function () {
+    if (this.newWatch.classList.contains('full')) {
         this.newWatch.innerHTML = this.createHhMmSsMsFormat()
-        document.body.prepend(this.newWatch);
-        this.newWatch.className = 'full'
-        this.newWatch.addEventListener('click', this.toggles.bind(this))
-    }
-
-    this.creatAllFormats = function () {
-        let date = new Date()
-        this.hh = date.getHours();
-        if (this.hh < 10) this.hh = '0' + this.hh;
-        this.mm = date.getMinutes();
-        if (this.mm < 10) this.mm = '0' + this.mm;
-        this.ss = date.getSeconds();
-        if (this.ss < 10) this.ss = '0' + this.ss;
-        this.ms = date.getMilliseconds();
-        if (this.ms < 10) this.ms = '00' + this.ms
-        if (this.ms >= 10 && this.ms < 100) this.ms = '0' + this.ms
-    }
-
-    this.createHhMmSsMsFormat = function () {
-        this.creatAllFormats()
-        return `${this.hh}:${this.mm}:${this.ss}:${this.ms}`
-    }
-
-    this.creatHhMmFormat = function () {
-        this.creatAllFormats()
-        return `${this.hh}:${this.mm}`
-    }
-
-    this.render = function () {
-        if (this.newWatch.classList.contains('full')) {
-            this.newWatch.innerHTML = this.createHhMmSsMsFormat()
-        } else {
-            this.newWatch.innerHTML = this.creatHhMmFormat()
-        }
-    }
-
-    this.startWatch = function () {
-        setInterval(() => this.render(), 250);
-    }
-
-    this.toggles = function () {
-        this.newWatch.classList.toggle('full')
+    } else {
+        this.newWatch.innerHTML = this.creatHhMmFormat()
     }
 }
 
-const WatchChild1 = function () {
-    this.createHhMmSsFormat = function () {
-        this.creatAllFormats()
-        return `${this.hh}:${this.mm}:${this.ss}`
-    }
-
-    this.creatHhMmFormat = function () {
-        this.creatAllFormats()
-        return `${this.hh}:${this.mm}`
-    }
+Watch.prototype.startWatch = function () {
+    setInterval(() => this.render(), 250);
 }
 
-const WatchChild2 = function () {
-    this.createHhMmFormat = function () {
-        this.creatAllFormats()
-        return `${this.hh}:${this.mm}`
-    }
-
-    this.creatHhMmSsMsFormat = function () {
-        this.creatAllFormats()
-        return `${this.hh}:${this.mm}:${this.ss}:${this.ms}`
-    }
+Watch.prototype.toggles = function () {
+    this.newWatch.classList.toggle('full')
 }
-let watch1 = new Watch1()
-watch1.startWatch()
-watch1.addElement()
 
-WatchChild1.prototype = new Watch1()
+let watch = new Watch('div')
+console.log(watch)
+watch.startWatch()
+watch.addElement()
+
+let WatchChild1 = function (elem) {
+    Watch.call(this, elem)
+}
+
+WatchChild1.prototype = Object.create(Watch.prototype);
 WatchChild1.prototype.render = function () {
     if (this.newWatch.classList.contains('full')) {
         this.newWatch.innerHTML = this.createHhMmSsFormat()
@@ -83,21 +72,24 @@ WatchChild1.prototype.render = function () {
     }
 }
 
-let watchChild1 = new WatchChild1()
+let watchChild1 = new WatchChild1('div')
 watchChild1.startWatch()
 watchChild1.addElement()
 
+let WatchChild2 = function (elem) {
+    Watch.call(this, elem)
+}
 
-WatchChild2.prototype = new Watch1()
+WatchChild2.prototype = Object.create(Watch.prototype);
 WatchChild2.prototype.render = function () {
     if (this.newWatch.classList.contains('full')) {
-        this.newWatch.innerHTML = this.createHhMmFormat()
+        this.newWatch.innerHTML = this.creatHhMmFormat()
     } else {
-        this.newWatch.innerHTML = this.creatHhMmSsMsFormat()
+        this.newWatch.innerHTML = this.createHhMmSsMsFormat()
     }
 }
 
-let watchChild2 = new WatchChild2()
+let watchChild2 = new WatchChild2('span')
 watchChild2.startWatch()
 watchChild2.addElement()
 
